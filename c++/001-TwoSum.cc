4d99b48ed9db3caadf6a5bc2151b03b0eb742cf4
haojia-Nie <52882728+haojia-Nie@users.noreply.github.com>
2020-06-05 19:00:37 -0400

    Update 001-TwoSum.cc

--- c++/001-TwoSum.cc
+++ c++/001-TwoSum.cc
@@ -1,17 +1,30 @@
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
+#include <iostream>
+
 using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
-            if (indices.find(target - nums[i]) != indices.end()) {
-                return {indices[target - nums[i]], i};
-            }
-            indices[nums[i]] = i;
-        }
-        return {};
-    }
+class Solution{
+	public:
+		vector<int> twoSum(vector<int>& nums, int target){
+			unordered_map<int,int> indicies;
+			int size = nums.size();
+			for (int i = 0; i < size; i ++) {
+				if(indicies.find(target - nums[i]) != indicies.end()) {
+					cout << "result is: " << indicies[target - nums[i]] << ' ' << i << endl;
+					return {indicies[target- nums[i]], i};
+				}
+				indicies[nums[i]] = i;
+			}
+			return {};
+		}
 };
+
+
+int main() {
+	Solution b;
+	vector<int> t1 = {1, 3, 4, 5, 6};
+	int target_1 = 5;
+	vector<int> result = b.twoSum(t1, target_1);
+	// do extra with it;
+}
